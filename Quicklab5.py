97029b3a33eeec674dcefc33f3ab255cc7203bfe
Sandy(CURDY)Afeawo <81618714+jasper-tech@users.noreply.github.com>
2022-04-30 12:33:16 +0000

    Add files via upload
    Converting Miles to kilometers
    Quick Recap to assigning values to variables

---
+++ Quicklab5.py
@@ -0,0 +1,14 @@
+#Miles to Kilometers
+#1 mile=1.61 kilometers
+
+kilometers=12.25
+miles= 7.38
+
+
+
+miles_to_Kilometers= float((miles*1.61)/1)
+Kilometers_to_miles= float((kilometers*1)/1.61)
+
+print(miles,"miles is",round(miles_to_Kilometers, 2), "Kilometers")
+print(kilometers, "Kilometers is", round(Kilometers_to_miles, 2),"miles")
+
