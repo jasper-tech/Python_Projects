980be8d2ff042e4d2360d0d32e7d46e7f876efac
Sandy(CURDY)Afeawo <81618714+jasper-tech@users.noreply.github.com>
2022-05-02 11:28:00 +0000

    Add files via upload
    Converting seconds to hours

---
+++ Quicklab7.py
@@ -0,0 +1,12 @@
+#Program Helps You convert seconds to hours
+
+
+a = 2  # number of hours
+b = 3600 # number of seconds per hour
+
+seconds = (a*b)  # printing the number of seconds
+
+print(seconds, "seconds is" ,a,"hours")  # printing number of seconds to a given hour
+
+
+print("Goodbye")
