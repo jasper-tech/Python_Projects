606b2e1ac144479cab559e44a3b656e2275a2673
Sandy(CURDY)Afeawo <81618714+jasper-tech@users.noreply.github.com>
2022-05-03 12:54:20 +0000

    Add files via upload
    Simple calculations in python using variables and assigning values to them!

---
+++ Quicklab8.py
@@ -0,0 +1,11 @@
+
+a = float(input("Enter A Value:"))
+b = float(input("Enter Another Value:"))
+
+add = a + b
+sub = a - b
+mult = a * b
+div = a / b
+
+print(add,sub,mult,div)
+print("\nsThats all, Folks!")
