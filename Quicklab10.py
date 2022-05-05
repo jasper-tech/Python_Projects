40ab2526217b283cc0b228c1ac0bc35dd23ed743
Sandy(CURDY)Afeawo <81618714+jasper-tech@users.noreply.github.com>
2022-05-05 08:22:10 +0000

    Add files via upload
    Calculation of time using if-else statements... Separated into hours , minutes and seconds

---
+++ Quicklab10.py
@@ -0,0 +1,15 @@
+hour = int(input("Starting time (hours) :"))
+minutes = int(input("Starting time (minutes) :"))
+duration = int(input("Event duration (minutes):"))
+
+
+time =  minutes +duration
+if time > 60:
+    cut=((time//60)+hour)%24
+    dug=duration+minutes % 60
+    
+    print(cut,":",dug)
+   
+    
+else:
+    print(hour,":",time)
