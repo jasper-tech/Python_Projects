e2c055143c5555e6eadd83437306aca154953e03
Sandy(CURDY)Afeawo <81618714+jasper-tech@users.noreply.github.com>
2022-05-06 19:29:50 +0000

    Add files via upload

--- Quicklab10.py
+++ Quicklab10.py
@@ -5,11 +5,11 @@ duration = int(input("Event duration (minutes):"))
 
 time =  minutes +duration
 if time > 60:
-    cut=((time//60)+ hour) % 24
+    cut=((time//60)+hour)%24
     dug=duration+minutes % 60
     
-    print(cut,"::",dug)
+    print(cut,":",dug)
    
     
 else:
-    print(hour,"::",time)
+    print(hour,":",time)
