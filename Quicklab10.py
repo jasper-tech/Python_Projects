0d92da1a2ed0f51e9ba87da093dcf5d97ebe6e7a
Sandy(CURDY)Afeawo <81618714+jasper-tech@users.noreply.github.com>
2022-05-07 16:27:48 +0000

    Update Quicklab10.py

--- Quicklab10.py
+++ Quicklab10.py
@@ -1,9 +1,12 @@
+#List your variables ie. hour,minutes,duration
 hour = int(input("Starting time (hours) :"))
 minutes = int(input("Starting time (minutes) :"))
 duration = int(input("Event duration (minutes):"))
 
 
+#Conversion of time
 time =  minutes +duration
+# if or else for cut and dug conversions
 if time > 60:
     cut=((time//60)+hour)%24
     dug=duration+minutes % 60
