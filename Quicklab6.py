3ee58cd71b452d0c9747fa85e3d3b1c63e3ab5d2
Sandy(CURDY)Afeawo <81618714+jasper-tech@users.noreply.github.com>
2022-05-07 17:07:27 +0000

    Update Quicklab6.py

--- Quicklab6.py
+++ Quicklab6.py
@@ -1,4 +1,6 @@
+#Inserting values into an equation
 x = float(input("Enter a Number:"))
-result=3*(x**3)-2*(x**2)+(3*x)-1
+result=3*(x**3)-2*(x**2)+(3*x)+1
 y= result
-print("y =", y)
+print("hence,y =", y)
+
