bb4d1c5708032b5254ce34cee0a8019768e4bf79
Sandy(CURDY)Afeawo <81618714+jasper-tech@users.noreply.github.com>
2022-05-08 12:57:37 +0000

    Add files via upload

---
+++ Quicklab12.py
@@ -0,0 +1,8 @@
+#Spathiphyllum
+
+a = str(input("A Plant Known As A peace Lily:"))
+
+if a == str('Spathiphyllum'):
+    print("Yes",a,"is the best plant ever!")
+else:
+    print("Not",a,"!","I want a big Spathiphyllum!")
