0fc8a35708f0ce94cbaad08319d9658764000787
Sandy(CURDY)Afeawo <81618714+jasper-tech@users.noreply.github.com>
2022-05-09 19:26:19 +0000

    Update Quicklab13.py

--- Quicklab13.py
+++ Quicklab13.py
@@ -1,15 +1,15 @@
 #Personal income tax
 
-
+#Defining variables
 income = float(input("Enter the annual income:"))
 fixed_amount = 85528
-
+#Using if-else for tax relief calculation
 if income <= fixed_amount:
     tax = (18/100*income)-556.2
     tax = round(tax,0)
-    print("Tax relief is", tax)
+    print(" Your Tax relief is", tax)
 else :
     tax = (income - fixed_amount)*0.32 + 14839.02
     tax = round(tax,0)
-    print ("Tax relief is",tax)
+    print (" Your Tax relief is",tax)
     
