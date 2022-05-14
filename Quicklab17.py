44e7cc42a39f300602903c7f7541671cfa27c07d
Sandy(CURDY)Afeawo <81618714+jasper-tech@users.noreply.github.com>
2022-05-14 21:39:28 +0000

    Add files via upload

---
+++ Quicklab17.py
@@ -0,0 +1,5 @@
+while True:
+    secret_word = str(input("Enter The secret word to exit this loop World:"))
+    if secret_word == "chupacabra":
+        print("oh no !!!!You've Escaped my world!")
+        break
