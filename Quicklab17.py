0b3c7490306a259ffbb67cf726cf731c3606975f
Sandy(CURDY)Afeawo <81618714+jasper-tech@users.noreply.github.com>
2022-05-15 08:19:55 +0000

    Delete Quicklab17.py

--- Quicklab17.py
+++
@@ -1,5 +0,0 @@
-while True:
-    secret_word = str(input("Enter The secret word to exit this loop World:"))
-    if secret_word == "chupacabra":
-        print("oh no !!!!You've Escaped my world!")
-        break
